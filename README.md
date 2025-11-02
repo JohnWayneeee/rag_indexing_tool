484c3671feba3d8d06d01fce8000ffb8c9a426ea
JohnWayne <miss@gmail.com>
2025-11-02 08:52:35 +0300

    Update LICENSE year to 2025, change Python requirement to 3.10 in setup.py, and update README badge for Python version

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RAG Indexing Tool
 
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
-[![Python 3.8+](https://img.shields.io/badge/python-3.8+-blue.svg)](https://www.python.org/downloads/)
+[![Python 3.10+](https://img.shields.io/badge/python-3.10+-blue.svg)](https://www.python.org/downloads/)
 [![FastAPI](https://img.shields.io/badge/FastAPI-0.104+-green.svg)](https://fastapi.tiangolo.com)
 
 Universal utility for indexing documents of various formats into a vector database with semantic search support. Converts documents to RAG-compatible format for use with LLMs.
--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     url="",
     packages=find_packages(where=".", include=["src*"]),
     package_dir={"": "."},
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     install_requires=requirements,
     entry_points={
         "console_scripts": [
@@ -40,8 +40,6 @@ setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
